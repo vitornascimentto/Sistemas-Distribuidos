3f0a49d29e5f4edcfc5021795bc46ed9110daff5
vitornascimentto <vitornascimentto0@gmail.com>
2019-08-26 19:23:30 -0300

    Algoritmo Valentao atualizado

---
+++ Algoritmo Valentão/README.txt
@@ -0,0 +1,5 @@
+Para executar o programa é preciso entrar, via terminal, no diretório 'SistemasDistribuidos'. Então executar o comando 'python3 main.py'.
+
+Alunos:
+	João Vítor do Nascimento
+	Igor Cavalcante
--- Algoritmo Valentão/main.py
+++ Algoritmo Valentão/main.py
@@ -1,3 +1,5 @@
+import random
+
 class Processo:
 
     def __init__(self, id, coordenador, vivo):
@@ -17,6 +19,9 @@ class Processo:
     def set_coordenador(self, coordenador):
         self.coordenador = coordenador
 
+    def set_vivo(self, vivo):
+        self.vivo = vivo
+
 def criar_processos(quantidade):
     
     lista_processos = []
@@ -31,18 +36,23 @@ def listar_processos(lista):
     for i in lista:
         print(i.__str__())
 
+    print()
+
 def eleicao(lista_processos, inicio):
     
     lista_id = [i.get_id() for i in lista_processos if i.get_vivo()]
     lista_participantes = [i for i in lista_id if i >= inicio]
 
+    print('Nó {} começou a eleição'.format(inicio))
+
     for i in range(len(lista_participantes)):
         if i != len(lista_participantes) - 1:
-            mensagem(lista_participantes[i], lista_participantes[i + 1])
-
+            for j in range(i + 1, len(lista_participantes)):
+                mensagem(lista_participantes[i], lista_participantes[j])
         else:
             mensagem(lista_participantes[i])
             print('nó {} é o novo coordenador'.format(lista_participantes[i]))
+            print()
             return lista_participantes[i]
 
 def mensagem(p1, p2=None):
@@ -63,21 +73,28 @@ def mensagem_coordenador(coordenador, lista_processos):
         else:
             i.set_coordenador(True)
 
+    print()
 
+def matar_processo(id, lista_processos):
+    
+    for i in lista_processos:
+        if i.get_id() == id:
+            print('nó {} parou de funcionar'.format(i.get_id()))
+            print()
+            i.set_vivo(False)
+
+    quantidade_processos = len(lista_processos) - 1
+    no_eleicao = random.randrange(0, quantidade_processos)
+
+    eleicao(lista_processos, no_eleicao)    
 
 if __name__ == "__main__":
-    lista_processos = criar_processos(5)
-
+    lista_processos = criar_processos(7)
     listar_processos(lista_processos)
 
-    coordenador = eleicao(lista_processos, 2)
-
+    coordenador = eleicao(lista_processos, 3)
     mensagem_coordenador(coordenador, lista_processos)
 
     listar_processos(lista_processos)
 
-
-
-
-
-    
+    matar_processo(6, lista_processos)
