21b3a02f3db9998bf746f5d0e9f5077161f5c6cf
vitornascimentto <vitornascimentto0@gmail.com>
2019-08-30 22:24:49 -0300

    Att Algoritmo Valentao

--- Algoritmo Valentão/main.py
+++ Algoritmo Valentão/main.py
@@ -2,25 +2,31 @@ import random
 
 class Processo:
 
-    def __init__(self, id, coordenador, vivo):
+    def __init__(self, id, coordenador, ativo):
         self.id = id
         self.coordenador = coordenador
-        self.vivo = vivo
+        self.ativo = ativo
+        self.processos = []
 
     def __str__(self):
-        return 'id: ' + str(self.id) + ' | coordenador: ' + str(self.coordenador) + ' | vivo: ' + str(self.vivo)
+        return 'Processo id: ' + str(self.id) + ' | coordenador: ' + str(self.coordenador) + ' | ativo: ' + str(self.ativo) + '\n' + 'Outros processos existentes: ' + str(self.processos) + '\n'
 
     def get_id(self):
         return self.id
 
-    def get_vivo(self):
-        return self.vivo
+    def get_ativo(self):
+        return self.ativo
 
     def set_coordenador(self, coordenador):
         self.coordenador = coordenador
 
-    def set_vivo(self, vivo):
-        self.vivo = vivo
+    def set_ativo(self, ativo):
+        self.ativo = ativo
+
+    def atualizar(self, lista_processos):
+        for i in lista_processos:
+            if i.get_id() != self.id:
+                self.processos.append(i.get_id())
 
 def criar_processos(quantidade):
     
@@ -36,14 +42,12 @@ def listar_processos(lista):
     for i in lista:
         print(i.__str__())
 
-    print()
-
 def eleicao(lista_processos, inicio):
     
-    lista_id = [i.get_id() for i in lista_processos if i.get_vivo()]
+    lista_id = [i.get_id() for i in lista_processos if i.get_ativo()]
     lista_participantes = [i for i in lista_id if i >= inicio]
 
-    print('Nó {} começou a eleição'.format(inicio))
+    print('Processo {} começou a eleição'.format(inicio))
 
     for i in range(len(lista_participantes)):
         if i != len(lista_participantes) - 1:
@@ -51,18 +55,18 @@ def eleicao(lista_processos, inicio):
                 mensagem(lista_participantes[i], lista_participantes[j])
         else:
             mensagem(lista_participantes[i])
-            print('nó {} é o novo coordenador'.format(lista_participantes[i]))
+            print('Processo {} é o novo coordenador'.format(lista_participantes[i]))
             print()
             return lista_participantes[i]
 
 def mensagem(p1, p2=None):
 
     if p2 == None:
-        p2 = 'nó inativo ou inexistente'
+        p2 = 'Processo inativo ou inexistente'
 
     print('{} -> {} (Eleição)'.format(p1, p2))
 
-    if p2 != 'nó inativo ou inexistente':
+    if p2 != 'Processo inativo ou inexistente':
         print('{} <- {} (OK)'.format(p1, p2))
 
 def mensagem_coordenador(coordenador, lista_processos):
@@ -79,17 +83,25 @@ def matar_processo(id, lista_processos):
     
     for i in lista_processos:
         if i.get_id() == id:
-            print('nó {} parou de funcionar'.format(i.get_id()))
+            print('Processo {} parou de funcionar'.format(i.get_id()))
             print()
-            i.set_vivo(False)
+            i.set_ativo(False)
 
     quantidade_processos = len(lista_processos) - 1
-    no_eleicao = random.randrange(0, quantidade_processos)
+    processo_eleicao = random.randrange(0, quantidade_processos)
 
-    eleicao(lista_processos, no_eleicao)    
+    eleicao(lista_processos, processo_eleicao)
+
+def att(lista_processos):
+    for i in lista_processos:
+        i.atualizar(lista_processos)
 
 if __name__ == "__main__":
     lista_processos = criar_processos(7)
+    
+    
+    att(lista_processos)
+
     listar_processos(lista_processos)
 
     coordenador = eleicao(lista_processos, 3)
